e7f651ee5233aaf45e9c8ff6dc4f0a06dd518894
Regis <regis.lopes.eso@gmail.com>
2025-12-08 11:48:42 -0300

    Global State Management with useState

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,31 @@
+<script setup>
+  const isLoggedIn = useIsLoggedIn();
+</script>
 <template>
   <div>
     <nav style="display: flex; gap: 10px">
       <NuxtLink to="/">Home</NuxtLink>
+
       <NuxtLink to="/movies">Movies</NuxtLink>
+
       <NuxtLink
         to="https://vueschool.io"
         target="_"
         >Vue School</NuxtLink
+      >
+
+      <NuxtLink
+        v-if="!isLoggedIn"
+        to="/login"
+        >Login</NuxtLink
+      >
+
+      <a
+        v-else
+        @click.prevent="isLoggedIn = false"
+        href="#"
+        to="/login"
+        >Logout Test</a
       >
     </nav>
     <slot></slot>
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,22 +1,32 @@
 <script setup>
+  const isLoggedIn = useIsLoggedIn();
+
   definePageMeta({
     layout: "plain",
   });
+
+  function login() {
+    isLoggedIn.value = true;
+    useRouter().push("/");
+  }
 </script>
 
 <template>
-  <form>
+  <form @submit.prevent="login">
     <h1>Login</h1>
     <label for="username">Username </label>
     <input
       type="text"
       id="username"
     />
-    <label for="password">Password </label>
-    <input
-      type="password"
-      id="password"
-    />
+    <label for="password"
+      >Password
+      <input
+        type="password"
+        id="password"
+      />
+    </label>
+    <button>LOGIN</button>
   </form>
 </template>
 
